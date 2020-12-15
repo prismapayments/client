40bec2d38ed62e0710c1d58ce01b47306f80c72b
Alejandro Moraga <moraga86@gmail.com>
2020-12-15 09:01:53 -0300

    updates

--- desk.js
+++ desk.js
@@ -1,8 +1,34 @@
 
 const HID = require('node-hid');
+const ThermalPrinter = require('node-thermal-printer').printer;
+const PrinterTypes = require('node-thermal-printer').types;
+
+let printer;
+
+try {
+    printer = new ThermalPrinter({
+        type: PrinterTypes.EPSON,
+        interface: 'printer:EPSON TM-T20X Receipt',
+        // driver: require('electron-printer'),
+        options: {
+            timeout: 1000
+        },
+        width: 48
+    });
+    
+    printer.isPrinterConnected().then(isConnected => {
+        console.log('Printer connected:', isConnected);
+    });
+}
+catch (e) {
+    console.log('printer error');
+}
+
+
 
 // console.log(HID.devices());
 
+/*
 const device = new HID.HID(8208, 30264);
 
 let buffs = [];
@@ -30,3 +56,39 @@ device.on('data', function (data) {
     // console.log(data.toString('ascii'));
     read(data);
 });
+*/
+
+const e = angular.element(document.body);
+
+e.ready(function () {
+
+    console.log('angular ready');
+    
+    const scope = e.scope();
+
+    scope.$on('print:order', async function (order) {
+        console.log('print order:', order);
+
+        try {
+            printer.println('Novo pedido');
+            printer.upsideDown(true);
+            printer.cut();
+
+            try {
+                await printer.execute();
+                console.log('Print success.');
+            }
+            catch (e) {
+                console.log('Print error:', error);
+            }
+        }
+        catch (e) {
+            console.log('Print main error:', error);
+        }
+
+    });
+})
+
+    
+
+
--- index.js
+++ index.js
@@ -1,46 +1,11 @@
 const { app, BrowserWindow } = require('electron');
-const http = require('http');
-const fs = require('fs');
-
-// const req = http.request({
-//     host: 'localhost', 
-//     port: 8080, 
-//     path: '/static/manager.html'
-// }, function (res) {
-//     res.setEncoding('utf-8'); 
-    
-//     let content = '';
-    
-//     res.on('data', function (chunk) { content += chunk; });
-
-//     const HOST = 'http://localhost:8080';
-    
-//     res.on('end', function () {
-//         const erase = ` 
-//         <script> 
-        
-//         window.HOST = '${HOST}';
-//         </script> 
-//         `;
-
-//         content = content.replace('<head>', '<head>' + erase);
-
-//         content = content.replace(/(href|src)="\//g, '$1="' + HOST + '/');
-//         console.log(content);
-
-//         fs.writeFileSync('./index.html', content);
-//         createWindow();
-//     })
-// });
-
-// req.end();
 
 let win;
 
 function createWindow() {
     win = new BrowserWindow({ 
         width: 1400, 
-        height: 600, 
+        height: 800, 
         webPreferences: { 
             nodeIntegration: true, 
             enableRemoteModule: false,
